e5840a4fc7a636a3235fcc4fd6a5dc0bed1a0709
Yaoyang Ding <ydin0039@student.monash.edu>
2023-08-06 12:26:34 +1000

    Add project files.

---
+++ 5032/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_5032.Startup))]
+namespace _5032
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
